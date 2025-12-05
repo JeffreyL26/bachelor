8437318951f1c305b5ab77a52faf98dea24ca114
JeffreyL26 <jeffrey.lehmann@t-online.de>
2025-12-05 08:51:48 +0100

    #2 Kommentare

--- pipeline_tester.py
+++ pipeline_tester.py
@@ -1,11 +1,11 @@
 from graph_pipeline import load_jsonl_as_pyg
 import os
 
-#TODO: Kommentare und Dokumentation revamp
+#TODO: Kommentare und Dokumentation revamp - Ziel 15.12
 
 if __name__ == "__main__":
     BASE = os.path.dirname(os.path.abspath(__file__))
-    ist_graphs = load_jsonl_as_pyg(os.path.join(BASE, "data", "ist_graphs.jsonl"), undirected=True)
+    ist_graphs = load_jsonl_as_pyg(os.path.join(BASE, "data", "ist_graphs.jsonl"), undirected=True) # Da symmetrische Verbindungen
     print("Anzahl Ist-Graphen:", len(ist_graphs))
     print("Erster Graph:", ist_graphs[0])
     print("x-Shape:", ist_graphs[0].x.shape)
