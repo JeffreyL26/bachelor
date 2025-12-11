cb4f36c19150318ac2adbe95a8e7cc88cd15da05
JeffreyL26 <jeffrey.lehmann@t-online.de>
2025-12-11 11:41:04 +0100

    #6 Kommentare

--- graph_templates.py
+++ graph_templates.py
@@ -4,8 +4,6 @@ import glob
 import json
 from typing import Dict, Any, List, Optional, Tuple
 
-#TODO: Kommentare und Dokumentation revamp - Ziel 15.12
-
 # Wir nutzen die Helfer aus graph_converter, damit Format identisch ist
 from graph_converter import make_node, make_edge, classify_pattern
 
